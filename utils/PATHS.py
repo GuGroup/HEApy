9b53700a222336a527ae97418ba6b1b3adc24342
ytkkk <ytk17927@kentech.ac.kr>
2025-12-09 22:50:44 +0900

    Update PATHS.py

--- utils/PATHS.py
+++ utils/PATHS.py
@@ -1,21 +1,3 @@
 from pathlib import Path
 
-test_paths=[
-Path("/home/ytk/HAE/DFTs/AlFeCoNiCu/slab_224/Ni_16/"),
-Path("/home/ytk/HAE/DFTs/AlFeCoNiCu/slab_224/Co_21/"),
-Path("/home/ytk/HAE/DFTs/AlFeCoNiCu/slab_224/Ni_28/"),
-Path("/home/ytk/HAE/DFTs/AlFeCoNiCu/slab_224/Al_26/"),
-Path("/home/ytk/HAE/DFTs/AlFeCoNiCu/slab_224/Fe_26/"),
-Path("/home/ytk/HAE/DFTs/AlFeCoNiCu/slab_224/Al_31/"),
-Path("/home/ytk/HAE/DFTs/AlFeCoNiCu/slab_224/Al_35/"),
-Path("/home/ytk/HAE/DFTs/AlFeCoNiCu/slab_224/Al_45/"),
-Path("/home/ytk/HAE/DFTs/AlFeCoNiCu/slab_224/Co_55/"),
-Path("/home/ytk/HAE/DFTs/AlFeCoNiCu/slab_224/Co_56/"),
-Path("/home/ytk/HAE/DFTs/AlFeCoNiCu/slab_224/Al_66/"),
-Path("/home/ytk/HAE/DFTs/AlFeCoNiCu/slab_224/Ni_65/"),
-Path("/home/ytk/HAE/DFTs/AlFeCoNiCu/slab_224/Fe_72/"),
-Path("/home/ytk/HAE/DFTs/AlFeCoNiCu/slab_224/Al_89/"),
-Path("/home/ytk/HAE/DFTs/AlFeCoNiCu/slab_224/Co_98/"),
-Path("/home/ytk/HAE/DFTs/AlFeCoNiCu/slab_224/Co_101/"),
-Path("/home/ytk/HAE/DFTs/AlFeCoNiCu/slab_224/Ni_92/"),
-]
+
